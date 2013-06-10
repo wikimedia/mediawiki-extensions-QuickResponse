b74ff6a4795aad704f395de1da95e96096ee4f70
raymond <raimond.spekking@gmail.com>
2013-06-10 14:46:33 +0200

    Some consistency tweaks in preparation for adding extension to translatewiki.net
    * Shorten description message
    * Remove . at end of the description message
    * Spaces2Tabs
    * Add standard headers
    
    Change-Id: I4988140d569022685c70ad9eedf6f9f1aeb4d6a8

--- QuickResponse.i18n.php
+++ QuickResponse.i18n.php
@@ -1,24 +1,32 @@
 <?php
-// Internationalisation for QuickResponse extension
- 
+/**
+ * Internationalisation file for the extension QuickResponse
+ *
+ * @file
+ * @ingroup Extensions
+ * @author konarak
+ * @licence MIT License
+ */
+
 $messages = array();
 
-// English
-// @author konarak
-
+/** English
+ * @author konarak
+ */
 $messages['en'] = array(
-    'quickresponse-desc' => 'This extension generates a QR code for the URL for the current page.',
-    'quickresponse-text' => 'QR code',
-    'quickresponse-tooltip' => 'Generate a QR code for the current URL',
-    'quickresponse-title' => 'Here\'s your QR code!'
+	'quickresponse-desc' => 'Generates a QR code for the URL for the current page',
+	'quickresponse-text' => 'QR code',
+	'quickresponse-tooltip' => 'Generate a QR code for the current URL',
+	'quickresponse-title' => 'Here\'s your QR code!'
 );
 
-// Message Documentation
-// @author konarak
-
+/** Message documentation (Message documentation)
+ * @author konarak
+ */
 $messages['qqq'] = array(
-    'quickresponse-desc' => 'Description of the extension',
-    'quickresponse-text' => 'Text for the link in the toolbox',
-    'quickresponse-tooltip' => 'Text for the link tooltip',
-    'quickresponse-title' => 'The title of the dialog box'
+	'quickresponse-desc' => 'Description of the extension',
+	'quickresponse-text' => 'Text for the link in the toolbox',
+	'quickresponse-tooltip' => 'Text for the link tooltip',
+	'quickresponse-title' => 'The title of the dialog box'
 );
+
